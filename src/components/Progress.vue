1dec1060bd0d98ed3baf7ec43c817e8aabf37942
Lachlan Miller <lachlan.miller.1990@outlook.com>
2019-12-01 14:24:32 +1000

    Remove code

--- src/components/Progress.vue
+++ src/components/Progress.vue
@@ -1,28 +1,14 @@
 <template>
   <div id="progress">
     {{ progress }}%
-    <div>Inner Height: {{ innerHeight }}</div>
-    <div>ScrollY: {{ scrollY }}</div>
-    <div>rectY: {{ rectY }}</div>
-    <div>document.scrollHeight: {{ documentHeight }}</div>
-    <div>document.scrollHeight - innerHeight: {{ documentHeight - innerHeight }}</div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-// function listener(this: Window, evt: Event): void {
-//   console.log('Scrolling!', window.scrollY + window.innerHeight)
-// }
-
 interface IData {
-  percent: string
   marker: HTMLElement | null
-  scrollY: number
-  innerHeight: number
-  documentHeight: number
-  rectY: number
   progress: string
   scrollEvent: ((this: Window, ev: Event) => void) | null
 }
@@ -39,53 +25,45 @@ export default Vue.extend({
       progress: '0',
       scrollEvent: null,
       marker: null,
-      percent: '0',
-      scrollY: 0,
-      documentHeight: 0,
-      innerHeight: 0,
-      rectY: 0
     }
   },
 
   methods: {
-    listener(): void {
+    listener() {
       if (!this.marker) {
-        return
+        const marker = this.getMarker()
+        if (!marker) {
+          return
+        }
+        this.marker = marker
       }
 
       const { y } = this.marker.getBoundingClientRect() as DOMRect
+      const progress =  100 - ((y - window.innerHeight) / (document.body.scrollHeight - window.innerHeight) * 100)
 
-      this.rectY = y
-      this.scrollY = window.scrollY
-      this.innerHeight = window.innerHeight
-      this.documentHeight = document.body.scrollHeight
-
-      const progress =  100 - ((this.rectY - this.innerHeight) / (this.documentHeight - this.innerHeight) * 100)
-
-      console.log(progress.toFixed(0))
       if (progress < 0) {
-        this.progress = '0'
-        // console.log(0)
-        return
+        return this.progress = '0'
       }
 
       if (progress > 100) {
-        this.progress = '100'
-        // console.log(1)
-        return
+        return this.progress = '100'
       }
 
       this.progress = progress.toFixed(0)
+    },
 
-      const currentScroll = window.scrollY
-      const distUntilMarkerOnScreen = window.scrollY - y
-      // console.log(distUntilMarkerOnScreen)
-      // this.percent = ((currentScroll / markerPosition) * 100).toFixed(0)
+    getMarker(): HTMLElement | null {
+      const marker = document.querySelector<HTMLElement>('#progress-marker')
+      if (marker) {
+        return marker
+      }
+
+      return null
     }
   },
 
   mounted() {
-    const marker = document.querySelector<HTMLElement>('#progress-marker')
+    const marker = this.getMarker()
     if (marker) {
       this.marker = marker
     }
@@ -98,26 +76,13 @@ export default Vue.extend({
 
     window.removeEventListener('scroll', this.scrollEvent)
   },
-
-  computed: {
-    // percent(): string {
-    //   console.log('Checking')
-    //   if (!this.marker) {
-    //     return '?'
-    //   }
-
-    //   const { y } = this.marker.getBoundingClientRect() as DOMRect
-
-    //   return ((y + window.scrollY) / y + window.scrollY).toFixed(0)
-    // },
-  },
 })
 </script>
 
 <style scoped>
 #progress {
   position: fixed;
-  right: 10px;
+  right: 50px;
   top: 10px;
 }
 </style>
